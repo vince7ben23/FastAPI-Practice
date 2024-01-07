cfa1fbcde4f918ef24a281def4eff1db5bbd8820
BenBen <vince7ben23@gmail.com>
2024-01-08 01:17:08 +0800

    update test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,38 @@
-from typing import Generator
+from typing import AsyncGenerator, Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
+from src.database.database import database
 from src.main import app
-from src.routers.post import comment_db, post_db
 
 
 @pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture()
 def client() -> Generator:
     yield TestClient(app)
 
 
 @pytest.fixture(autouse=True)
-def db() -> Generator:
-    post_db.clear()
-    comment_db.clear()
+async def db() -> AsyncGenerator:
+    await database.connect()
+    await reset_increment_seq()
     yield
+    await database.disconnect()
+
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+        yield ac
+
+
+async def reset_increment_seq():
+    # await database.execute("TRUNCATE TABLE posts, comments RESTART IDENTITY;")
+    await database.execute("ALTER SEQUENCE posts_id_seq RESTART WITH 1;")
+    await database.execute("ALTER SEQUENCE comments_id_seq RESTART WITH 1;")
--- tests/routers/test_post.py
+++ tests/routers/test_post.py
@@ -1,71 +1,107 @@
-from fastapi.testclient import TestClient
+from typing import AsyncGenerator
+
+import pytest
 from requests import Response
 
 
 class TestPost:
-    def test_create_post(self, client: TestClient) -> None:
-        response = self._create_post(client)
-
+    @pytest.mark.anyio
+    async def test_create_post(self, async_client: AsyncGenerator) -> None:
+        response = await async_client.post("/post", json={"body": "test post"})
         assert response.status_code == 201
-        assert {"id": 0, "body": "test post"}.items() <= response.json().items()
+        assert {"id": 1, "body": "test post"}.items() <= response.json().items()
 
-    def test_get_post(self, client: TestClient) -> None:
-        self._create_post(client)
-
-        response = client.get("/post/0")
+    @pytest.mark.anyio
+    async def test_get_post(
+        self, async_client: AsyncGenerator, _create_post: Response
+    ) -> None:
+        response = await async_client.get(
+            "/post/1",
+        )
         assert response.status_code == 200
-        assert response.json() == {"id": 0, "body": "test post"}
+        assert response.json() == _create_post.json()
 
-    def test_get_post_id_not_in_db(self, client: TestClient) -> None:
-        response = client.get("/post/0")
+    @pytest.mark.anyio
+    async def test_get_post_id_not_in_db(self, async_client: AsyncGenerator) -> None:
+        response = await async_client.get("/post/1")
         assert response.status_code == 200
         assert response.json() is None
 
-    def test_get_posts(self, client: TestClient) -> None:
-        self._create_post(client)
-        response = client.get("/posts")
+    @pytest.mark.anyio
+    async def test_get_posts(
+        self, async_client: AsyncGenerator, _create_post: Response
+    ) -> None:
+        response = await async_client.get("/posts")
         assert response.status_code == 200
-        assert response.json() == [{"id": 0, "body": "test post"}]
+        assert response.json() == [_create_post.json()]
 
-    def test_create_comment(self, client: TestClient) -> None:
-        self._create_post(client)
-        response = self._create_comment(client)
+    @pytest.mark.anyio
+    async def test_create_comment(
+        self, async_client: AsyncGenerator, _create_post: Response
+    ) -> None:
+        response = await async_client.post(
+            "/comment", json={"body": "test comment", "post_id": 1}
+        )
         assert response.status_code == 201
         assert {
-            "id": 0,
+            "id": 1,
             "body": "test comment",
-            "post_id": 0,
+            "post_id": _create_post.json()["id"],
         }.items() <= response.json().items()
 
-    def test_create_comment_no_post_id(self, client: TestClient) -> None:
-        response = self._create_comment(client)
+    @pytest.mark.anyio
+    async def test_create_comment_no_post_id(
+        self, async_client: AsyncGenerator
+    ) -> None:
+        response = await async_client.post(
+            "/comment", json={"body": "test comment", "post_id": 1}
+        )
         assert response.status_code == 404
 
-    def test_get_comments_by_post_id(self, client: TestClient):
-        self._create_post(client)
-        self._create_comment(client)
-        response = client.get("/post/0/comment")
-        assert response.status_code == 200
-        assert {"post_id": 0, "id": 0, "body": "test comment"} in response.json()
-
-    def test_get_post_with_comments(self, client: TestClient):
-        self._create_post(client)
-        self._create_comment(client)
-        response = client.get("/post_with_comments/0")
+    @pytest.mark.anyio
+    async def test_get_comments_by_post_id(
+        self,
+        async_client: AsyncGenerator,
+        _create_post: Response,
+        _create_comment: Response,
+    ) -> None:
+        response = await async_client.get("/post/1/comment")
         assert response.status_code == 200
         assert {
-            "post": {"id": 0, "body": "test post"},
-            "comments": [{"post_id": 0, "id": 0, "body": "test comment"}],
+            "post_id": _create_post.json()["id"],
+            "id": _create_comment.json()["id"],
+            "body": "test comment",
+        } in response.json()
+
+    @pytest.mark.anyio
+    async def test_get_post_with_comments(
+        self,
+        async_client: AsyncGenerator,
+        _create_post: Response,
+        _create_comment: Response,
+    ) -> None:
+        response = await async_client.get("/post_with_comments/1")
+        assert response.status_code == 200
+        assert {
+            "post": _create_post.json(),
+            "comments": [_create_comment.json()],
         }.items() <= response.json().items()
 
-    def test_get_post_with_comments_no_post_id(self, client: TestClient):
-        response = client.get("/post_with_comments/0")
+    @pytest.mark.anyio
+    async def test_get_post_with_comments_no_post_id(
+        self, async_client: AsyncGenerator
+    ) -> None:
+        response = await async_client.get("/post_with_comments/1")
         assert response.status_code == 404
 
-    def _create_post(self, client: TestClient) -> Response:
-        response = client.post("/post", json={"body": "test post"})
+    @pytest.fixture()
+    async def _create_post(self, async_client: AsyncGenerator) -> Response:
+        response = await async_client.post("/post", json={"body": "test post"})
         return response
 
-    def _create_comment(self, client: TestClient) -> Response:
-        response = client.post("/comment", json={"body": "test comment", "post_id": 0})
+    @pytest.fixture()
+    async def _create_comment(self, async_client: AsyncGenerator) -> Response:
+        response = await async_client.post(
+            "/comment", json={"body": "test comment", "post_id": 1}
+        )
         return response
