f7300c5e15f3af6d56874c817517cff9da3fe39b
BenBen <vince7ben23@gmail.com>
2024-01-08 01:16:44 +0800

    api request from db

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from src.database.database import database
 from src.routers.post import post_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(post_router)
--- src/routers/post.py
+++ src/routers/post.py
@@ -2,6 +2,7 @@ from typing import List, Union
 
 from fastapi import APIRouter, HTTPException
 
+from src.database.database import database
 from src.models.post import (
     Comment,
     CommentIn,
@@ -13,55 +14,60 @@ from src.models.post import (
 post_router = APIRouter()
 
 
-post_db = {}
-
-
 @post_router.post("/post", status_code=201)
-def create_post(post: UserPostIn) -> UserPost:
+async def create_post(post: UserPostIn) -> UserPost:
     data = post.model_dump()
-    id = len(post_db)
-    new_post = {"id": id, **data}
-    post_db[id] = new_post
-    return new_post
+    query = f"INSERT INTO posts(body) VALUES('{data['body']}') RETURNING id;"
+    last_post_id = await database.execute(query)
+    return {"id": last_post_id, **data}
 
 
 @post_router.get("/post/{id}")
-def get_post(id: int) -> Union[None, UserPost]:
-    return post_db.get(id)
+async def get_post(id: int) -> Union[None, UserPost]:
+    query = f"SELECT * FROM posts WHERE id={id};"
+    res = await database.fetch_one(query)
+    return res
 
 
 @post_router.get("/posts")
-def get_posts() -> List[UserPost]:
-    return list(post_db.values())
-
-
-comment_db = {}
+async def get_posts() -> List[UserPost]:
+    query = "SELECT id, body FROM posts;"
+    res = await database.fetch_all(query)
+    return res
 
 
 @post_router.post("/comment", status_code=201)
-def create_comment(comment: CommentIn) -> Comment:
-    post = get_post(comment.post_id)
+async def create_comment(comment: CommentIn) -> Comment:
+    post = await get_post(comment.post_id)
     if not post:
         raise HTTPException(404, "can not find the post.")
     data = comment.model_dump()
-    comment_id = len(comment_db)
-    new_comment = {"id": comment_id, **data}
-    comment_db[comment_id] = new_comment
-    return new_comment
+    query = f"""
+            INSERT INTO comments(body, post_id) 
+            VALUES('{data['body']}', {data['post_id']})
+            RETURNING id
+            ;
+        """
+    last_comment_id = await database.execute(query)
+    return {"id": last_comment_id, **data}
 
 
 @post_router.get("/post/{post_id}/comment")
-def get_comments_by_post_id(post_id: int) -> List[Comment]:
-    comments = [
-        comment for comment in comment_db.values() if comment["post_id"] == post_id
-    ]
-    return comments
+async def get_comments_by_post_id(post_id: int) -> List[Comment]:
+    query = f"""
+        SELECT id, post_id, body 
+        FROM comments 
+        WHERE post_id={post_id}
+        ;
+    """
+    res = await database.fetch_all(query)
+    return res
 
 
 @post_router.get("/post_with_comments/{post_id}")
-def get_post_with_comments(post_id: int) -> UserPostWithComments:
-    post = get_post(post_id)
+async def get_post_with_comments(post_id: int) -> UserPostWithComments:
+    post = await get_post(post_id)
     if not post:
         raise HTTPException(404, "can not find the post.")
-    comments = get_comments_by_post_id(post_id)
+    comments = await get_comments_by_post_id(post_id)
     return {"post": post, "comments": comments}
